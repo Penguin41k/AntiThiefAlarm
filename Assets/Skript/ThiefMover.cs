3fa739ee3d1aa8d85079ab5726ee449d1686e592
Penguin41k <106864254+Penguin41k@users.noreply.github.com>
2023-06-24 16:50:48 +0500

    Исправлено 3

--- Assets/Skript/ThiefMover.cs
+++ Assets/Skript/ThiefMover.cs
@@ -1,16 +1,18 @@
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class ThiefMover : MonoBehaviour
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
+class ThiefMover : MonoBehaviour
 {
     [SerializeField] private float _speed;
-        
+
     private float _xAxisMoveDirection;
     private SpriteRenderer _spriteRenderer;
     private Animator _animator;
     private string _idel = "Idel";
     private string _walk = "Walk";
-       
+
     private void Start()
     {
         _animator = GetComponent<Animator>();
@@ -20,11 +22,11 @@ public class ThiefMover : MonoBehaviour
     private void Update()
     {
         _xAxisMoveDirection = Input.GetAxis("Horizontal");
-        _spriteRenderer.flipX = _xAxisMoveDirection == 0 ? _spriteRenderer.flipX : _xAxisMoveDirection < 0 ;
-        
+        _spriteRenderer.flipX = _xAxisMoveDirection == 0 ? _spriteRenderer.flipX : _xAxisMoveDirection < 0;
+
         if (_xAxisMoveDirection != 0)
         {
-            transform.Translate(_xAxisMoveDirection*_speed * Time.deltaTime, 0, 0);
+            transform.Translate(_xAxisMoveDirection * _speed * Time.deltaTime, 0, 0);
             _animator.SetTrigger(_walk);
         }
         else
